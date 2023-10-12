019c5e61ce3e7283a3b16222ab32ddb8d4bfb7b6
AnKub <ankub3088@gmail.com>
2023-10-12 17:03:09 +0300

    done?

--- src/route/test.js
+++ src/route/test.js
@@ -2,26 +2,21 @@
 const express = require('express')
 // Cтворюємо роутер - місце, куди ми підключаємо ендпоїнти
 const router = express.Router()
-
 const Test = require('../class/test')
-
 // ================================================================
-
 // router.get Створює нам один ентпоїнт
-
 // ↙️ тут вводимо шлях (PATH) до сторінки
 router.get('/', function (req, res) {
   // res.render генерує нам HTML сторінку
-
   // ↙️ cюди вводимо назву файлу з сontainer
   res.render('index', {
     // вказуємо назву контейнера
     name: 'index',
     // вказуємо назву компонентів
-    component: ['heading'],
+    component: [],
 
     // вказуємо назву сторінки
-    title: 'Назва сторінки',
+    title: 'To-do list',
     // ... сюди можна далі продовжувати додавати потрібні технічні дані, які будуть використовуватися в layout
 
     // вказуємо дані,
@@ -31,6 +26,5 @@ router.get('/', function (req, res) {
   })
   // ↑↑ сюди вводимо JSON дані
 })
-
 // Підключаємо роутер до бек-енду
 module.exports = router
